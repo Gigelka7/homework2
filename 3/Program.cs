e885c86a3d12cf4dba2ac69316c5111a12a59496
Kolesnik Anton <kolesnikas2006@gmail.com>
2022-11-01 18:29:41 +0500

    228

---
+++ 3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+int[] a = {1,2,3,4,5};
+int c = 8;
+if (c == a[0])
+{
+    Console.WriteLine("да");
+}
+else if (c == a[1])
+{
+    Console.WriteLine("да");
+}
+else if (c == a[2])
+{
+    Console.WriteLine("да");
+}
+else if (c == a[3])
+{
+    Console.WriteLine("да");
+}
+else if (c == a[4])
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
